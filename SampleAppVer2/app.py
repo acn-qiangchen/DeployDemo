0023a67d99228ca7ced7adc9e543d28ee3d07c96
shearertink <shearertink@gmail.com>
2017-07-23 17:28:15 +0900

    show hostname added

--- SampleAppVer2/app.py
+++ SampleAppVer2/app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+    return render_template('index.html', hostname=request.host)
 
 
 if __name__ == "__main__":
