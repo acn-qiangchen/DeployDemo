6c2a823cc5c48f3f7ef4d83a96839b94878252ac
shearertink <shearertink@gmail.com>
2017-08-06 10:33:52 +0900

    show server host

--- SampleAppVer2/app.py
+++ SampleAppVer2/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request
+import socket
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template('index.html', hostname=request.host)
+    return render_template('index.html', hostname=socket.gethostname())
 
 
 if __name__ == "__main__":
